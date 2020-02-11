f98bcd283aa0286c177c3aef3e8e3c130906759b
Jakub Kraus <poczta.jakub1@gmail.com>
2020-02-11 19:17:48 +0100

    Functionality done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
-window.addEventListener("keydown", function(e) {
-  const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if (!audio) return;
-  console.log(audio);
-  audio.currentTime = 0;
-  audio.play();
-});
+window.onload = function(e) {
+  playSound(e);
+  removeTransition(e);
+};
+
+//Sound and transition
+
+window.addEventListener(
+  "keydown",
+  (playSound = e => {
+    //Sound
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+
+    //Start transition
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.add("playing");
+  })
+);
+
+//End transition
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
