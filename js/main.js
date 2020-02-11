6e9344969f0a5b5609bb1802b81c4731c1e1792f
Jakub Kraus <poczta.jakub1@gmail.com>
2020-02-11 18:12:34 +0100

    You can click how many times you like now

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ window.addEventListener("keydown", function(e) {
   const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
   if (!audio) return;
   console.log(audio);
+  audio.currentTime = 0;
   audio.play();
 });
